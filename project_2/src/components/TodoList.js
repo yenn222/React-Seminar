82154e730625f3b80e3a7ea7f99846719d1a3f47
yenn222 <20221038@o365.hanbat.ac.kr>
2024-01-10 21:24:19 +0900

    project2_src

---
+++ project_2/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import "./TodoList.css";
+
+const TodoList = () => {
+    return(
+        <div className="TodoList">
+            <h4>Todo List 🌱</h4>
+            <input className="searchbar" placeholder="검색어를 입력하세요"/>
+        </div>
+    );
+};
+
+export default TodoList;
+
