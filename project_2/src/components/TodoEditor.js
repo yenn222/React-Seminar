5a34101cbc2e450f6ec4a120ac8bbc3f67640347
yenn222 <20221038@o365.hanbat.ac.kr>
2024-01-11 10:22:34 +0900

    project2_src_fin

--- project_2/src/components/TodoEditor.js
+++ project_2/src/components/TodoEditor.js
@@ -1,12 +1,36 @@
 import "./TodoEditor.css";
+import {useRef, useState} from "react";
 
-const TodoEditor = () => {
+const TodoEditor = ({ onCreate }) => {
+    const [content, setContent] = useState("");
+    const inputRef = useRef();
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    }
+    const onSubmit = () => {
+        if(!content) {
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    };
+    const onKeyDown = (e) => {
+        if(e.keyCode === 13){
+            onSubmit();
+        }
+    }
     return (
         <div className="TodoEditor">
             <h4>새로운 Todo 작성하기 ✏️</h4>
             <div className="editor_wrapper">
-                <input placeholder="새로운 Todo..."/>
-                <button>추가</button>
+                <input
+                    ref = {inputRef}
+                    value = {content}
+                    onChange={onChangeContent}
+                    onKeyDown={onKeyDown}
+                    placeholder="새로운 Todo..."/>
+                <button onClick={onSubmit}>추가</button>
             </div>
         </div>
     );
--- project_2/src/components/TodoItem.js
+++ project_2/src/components/TodoItem.js
@@ -1,15 +1,25 @@
 import "./TodoItem.css";
 
-const TodoItem = () => {
+const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete}) => {
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    };
+
+    const onClickDelete = () => {
+        onDelete(id);
+    }
+
     return (
         <div className="TodoItem">
             <div className="checkbox_col">
-                <input type="checkbox" />
+                <input onChange={onChangeCheckbox}
+                       checked={isDone}
+                       type="checkbox" />
             </div>
-            <div className="title_col">할 일</div>
+            <div className="title_col">{ content }</div>
             <div className="date_col">{new Date().toLocaleDateString()}</div>
             <div className="btn_col">
-                <button>삭제</button>
+                <button onClick={onClickDelete}>삭제</button>
             </div>
         </div>
     );
