bc364fc7f87571ce9008ebb0ca17e5d3f635ee46
yenn222 <20221038@o365.hanbat.ac.kr>
2024-01-14 17:58:30 +0900

    rename

--- train/src/components/Controller.js
+++
@@ -1,9 +0,0 @@
-const Controller = ({ handelSetCount }) => {
-    return(
-        <div>
-            <button onClick={() => handelSetCount()}>+</button>
-        </div>
-    );
-};
-
-export default Controller;
